c202581b8f1afe9b2990b066692459d036a891ed
matty9090 <lowe.matthew@mail.com>
2017-09-21 14:35:24 +0100

    Object inheritance Window caption

--- Breakout.cpp
+++ Breakout.cpp
@@ -4,6 +4,8 @@
 #include <TL-Engine.h>
 
 #include <string>
+#include <vector>
+#include <memory>
 
 using namespace tle;
 using namespace std;
@@ -13,17 +15,57 @@ using namespace std;
 const int kScreenWidth = 1024;
 const int kScreenHeight = 768;
 
+const string kGameName = "Breakout";
 const string kMediaFolder = "res";
 const EEngineType kEngineType = kTLX;
 
+/* 2D Vector */
+
+template <class T> struct Vec {
+	T x;
+	T y;
+
+	Vec() { x = 0; y = 0; }
+	Vec(T u, T v) { x = u, y = v; }
+};
+
+/* Object with position + velocity */
+
+class DynamicObj {
+	public:
+		Vec<float> getPos() { return position; }
+		Vec<float> getVel() { return velocity; }
+
+		void setPos(Vec<float> p) { position = p; }
+		void setVel(Vec<float> v) { velocity = v; }
+
+	protected:
+		Vec<float> position;
+		Vec<float> velocity;
+};
+
 /* Player/Bat */
 
-class Bat {
+class Bat : public DynamicObj {
 	public:
+		Bat() {
 
+		}
 
 	private:
+		
+};
 
+/* Destructable Block */
+
+class Block : public DynamicObj {
+	public:
+		Block() {
+
+		}
+
+	private:
+		
 };
 
 /* Game class */
@@ -38,6 +80,7 @@ class Breakout {
 
 		void setupEngine() {
 			engine->StartWindowed(kScreenWidth, kScreenHeight);
+			engine->SetWindowCaption(kGameName);
 			engine->AddMediaFolder(kMediaFolder);
 		}
 
@@ -48,8 +91,10 @@ class Breakout {
 		}
 
 	private:
-		Bat player;
 		I3DEngine *engine;
+
+		shared_ptr<Bat> player;
+		vector<shared_ptr<Block>> blocks;
 };
 
 /* Entry point */
